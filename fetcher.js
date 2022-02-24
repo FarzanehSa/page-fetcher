f4488e944af9ca00685af20ee4781f6bdb441e0e
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-02-24 14:01:18 -0800

    update fetcher

--- fetcher.js
+++ fetcher.js
@@ -2,18 +2,31 @@ const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
 
+// write function
+const writeBody = (path,body) => {
+  fs.writeFile(path,body,err => {
+    // Path is not valid
+    if (err && err.code === 'ENOENT') {
+      console.log('Path not found');
+      return;
+    }
+    //file written successfully
+    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+  });
+};
 
+// ---------------------------------------------------
 
 const fetcher = function(url,path) {
   
-
   request(url, (error, response, body) => {
     
     // URL results in an error or non-200 result
-    if (response.statusCode !== 200) {
-      console.log('URL is invalid');
+    if (error) {
+      console.log("Can't download URL");
       return;
     }
+    
     // file path already exists
     if (fs.existsSync(path)) {
       const rl = readline.createInterface({
@@ -23,35 +36,32 @@ const fetcher = function(url,path) {
       rl.question('Path already exit! Want to over-write? y / anyother key    ', (answer) => {
         rl.close();
         if (answer === 'y') {
-          fs.writeFile(path,body,err => {
-            if (err) {
-              console.log(err);
-              return;
-            }
-            console.log('overwrite done');
-            return;
-          });
+          writeBody(path,body);
         } else {
           console.log('see you next time!');
           return;
         }
       });
-    
+      
     } else {
-      fs.writeFile(path,body,err => {
-        // Path is not valid
-        if (err.errno === -2) {
-          console.log('Path not found');
-          return;
-        }
-        //file written successfully
-        console.log('done');
-        return;
-      });
+      writeBody(path,body);
     }
   });
 };
 
-fetcher('http://www.example.edu','./index.txt');
-// fetcher('http://www.example.edu','./a/index.txt');
-// fetcher('https://www.google.com/fdsafsafsa.html','./index.txt');
+// ------------------------------------------
+
+const input = process.argv.slice(2);
+if (input.length !== 2) {
+  console.log('\nWe are going to Download and Save');
+  console.log('After app name, insert URL & Path \n');
+  return;
+}
+
+const url = input[0];   //  http://www.example.edu 
+const path = input[1];  //  ./index.txt
+
+fetcher(url,path);
+
+
+
